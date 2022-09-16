50626f0f666263b0642ddfb5ddb2e5b6c26541f7
Wilson Wang <wilson.wang@bytedance.com>
2022-09-16 00:45:02 +0000

    use wasi-sdk

--- tools/lkl/build.sh
+++ tools/lkl/build.sh
@@ -5,8 +5,9 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 if [ "${DOCKER}" == "1" ]
 then
-    source /emsdk/emsdk_env.sh
-    make -C tools/lkl CC="clang" SHELL="bash -x" -j ${CPU_AMOUNT}
+    #source /emsdk/emsdk_env.sh
+    export PATH=/opt/wasi-sdk/bin:${PATH}
+    make -C tools/lkl CC="clang -emit-llvm --target=wasm32 " SHELL="sh -x" -j ${CPU_AMOUNT}
 else
     docker run -it --rm -e DOCKER=1 -e CPU_AMOUNT=$(nproc) -v ${SCRIPT_DIR}/../../:/root/lkl/:rw lkl-build:latest ./tools/lkl/build.sh
 fi
